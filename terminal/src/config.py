3d6c8fed5685074f374c84b6469c7f0493bf3826
Rocky <brennanhilton2201@gmail.com>
2024-06-14 03:19:59 -0400

    Terminal Settings

---
+++ terminal/src/config.py
@@ -0,0 +1,16 @@
+settings = {
+    'font_size': 12,
+    'font_color': 'white',
+    'background_color': 'black',
+    'transparency': True,
+    'transparency_level': 0.8,
+    'font_family': 'Consolas',
+    'cursor_shape': 'block',  # Options: 'block', 'underline', 'ibeam'
+    'cursor_blink': True,
+    'scroll_speed': 1.0,
+    'text_antialiasing': True,
+    'padding': 10,
+    'margin': 5
+}
+
+# Will be used later on for customizable terminal settings
